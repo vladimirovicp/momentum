b60c4e5e792f8cc19ac07781c0ee369e996f1730
vladimirovicp <vladimirovicp70@gmail.com>
2022-07-14 14:36:27 +0400

    feat: add day and Enter name

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
-
 const time = document.querySelector('.time');
 const date_time = document.querySelector('.date');
-const options = {month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC'};
+const greeting = document.querySelector('.greeting');
+let nameUser = document.querySelector('.name');
 
-const timeOptions = {hour: 'numeric', minute: 'numeric', second: 'numeric'};
-const dateOptions = {weekday: 'long' , month:'long',  day:'numeric'};
+const changeQuote = document.querySelector('.change-quote');
+
+// const options = { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC' };
+
+const timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+const dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
 
 
 // console.log(time);
@@ -23,12 +27,11 @@ function showTime() {
 
     setTimeout(showTime, 1000);
     showDate();
+    showGreeting();
 }
-
 //  'ru-RU', 'en-US', 'en-Br'
 
-
-function showDate(){
+function showDate() {
     const date = new Date();
     // const options = {month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC'};
     // const options = {month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC'};
@@ -36,8 +39,40 @@ function showDate(){
     date_time.textContent = currentDate;
 }
 
+function showGreeting() {
+    const date = new Date();
+    const hours = date.getHours();
+    let welcomeText = 'night';
 
-
-
+    if (hours >= 6 && hours < 12) {
+        welcomeText = 'morning';
+    } else if (hours >= 12 && hours < 18) {
+        welcomeText = 'day';
+    } else if (hours >= 18 && hours < 24) {
+        welcomeText = 'evening';
+    }
+    greeting.textContent = `Good ${welcomeText}`;
+}
 
 showTime();
+
+
+// перед перезагрузкой или закрытием страницы (событие beforeunload) данные нужно сохранить
+function setLocalStorage() {
+    localStorage.setItem('name', nameUser.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+// перед загрузкой страницы (событие load) данные нужно восстановить и отобразить
+function getLocalStorage() {
+    if (localStorage.getItem('name')) {
+        nameUser.value = localStorage.getItem('name');
+    }
+}
+window.addEventListener('load', getLocalStorage)
+
+
+
+// changeQuote.addEventListener('click', function() {
+//     console.log(nameUser.value);
+// });
