40a83dbb8c32bcf43608fb7c4cf940f45263bda0
vladimirovicp <vladimirovicp70@gmail.com>
2022-07-27 14:14:47 +0400

    feat: lang v2

--- js/script.js
+++ js/script.js
@@ -8,20 +8,15 @@ const body = document.querySelector('body');
 
 const changeQuote = document.querySelector('.change-quote');
 
-// const options = { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC' };
 
-const timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
-const dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
 
 let numberSlide = 1;
-
 const slideNext = document.querySelector('.slide-next');
 const slidePrev = document.querySelector('.slide-prev');
 let timeOfDay;
 
 const city = document.querySelector('.city');
 city.addEventListener('change', () => {
-    // console.log(city.value);
     getWeather();
 });
 
@@ -30,68 +25,23 @@ const wind = document.querySelector('.wind');
 const humidity = document.querySelector('.humidity');
 
 
-
 const quotesQuote = document.querySelector('.quote');
 const quotesAuthor = document.querySelector('.author');
 const quotesButton = document.querySelector('.change-quote');
 
+let lang = localStorage.getItem('lang');
 
-// language
+// const options = { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC' };
 
-// let lang = 'ru';
+let dateOptions;
 
-// function getLocalStorageLang() {
-//     let lang = localStorage.getItem('lang');
-//     if(lang === null){
-//         getTranslate('en');
-//
-//     } else {
-//         getTranslate(lang);
-//     }
-// }
-
-// window.addEventListener('load', getLocalStorage);
-
-// let lang = 'ru';
- let lang = localStorage.getItem('lang');
-
- console.log(localStorage.getItem('lang'));
-
-// if(lang === null){
-//     getTranslate('en');
-// } else {
-//     getTranslate(lang);
-// }
-
-
-
-/* Translate */
-
-let currentlanguage = document.querySelector('.language');
-
-currentlanguage.addEventListener('click', (e) => {
-    // getTranslate(e.target.innerText);
-    localStorage.setItem('lang',e.target.innerText);
-    lang = e.target.innerText;
-
-    currentlanguage.querySelectorAll('li a').forEach(el => {
-        el.classList.toggle('active');
-    })
-
-    // localStorage.setItem('lang', lang);
-    // console.log(localStorage.getItem('lang'));
-
-});
-
-// function getTranslate(language) {
-//     const i18 = document.querySelectorAll('[data-i18]');
-//     i18.forEach((value) => {
-//         const text = value.dataset.i18;
-//         value.textContent = i18Obj[language][text];
-//     });
-// }
-
-//
+if(lang === 'ru'){
+    dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
+} else {
+    dateOptions = { month: 'long', day: 'numeric', weekday: 'long'};
+}
+const timeOptions = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
+// const dateOptions = { weekday: 'long', month: 'long', day: 'numeric' };
 
 function showLangFormatTime() {
     let showTimeLang;
@@ -100,7 +50,7 @@ function showLangFormatTime() {
             showTimeLang = 'ru-RU';
             break;
         default:
-            showTimeLang = 'ru-RU';
+            showTimeLang = 'en-En';
     }
     return showTimeLang;
 }
@@ -144,7 +94,7 @@ function getTimeOfDay() {
 
 function showGreeting() {
     let welcomeText;
-    if (lang = 'ru') {
+    if (lang === 'ru') {
         switch (getTimeOfDay()) {
             case 'morning':
                 welcomeText = 'Доброе утро';
@@ -161,9 +111,14 @@ function showGreeting() {
             default:
                 welcomeText = '';
         }
+
+        greeting.textContent = welcomeText;
+
+    } else {
+        greeting.textContent = `Good ${getTimeOfDay()}`;
     }
     // greeting.textContent = `Good ${getTimeOfDay()}`;
-    greeting.textContent = welcomeText;
+    // greeting.textContent = welcomeText;
 }
 
 showTime();
@@ -186,17 +141,9 @@ function getLocalStorage() {
     if (localStorage.getItem('city')) {
         city.value = localStorage.getItem('city');
     }
-
-    lang = localStorage.getItem('lang');
-
-    // if (lang === null) {
-    //     getTranslate('en');
-    //
-    // } else {
-    //     getTranslate(lang);
-    // }
-
-
+    if (localStorage.getItem('lang')) {
+        lang = localStorage.getItem('lang');
+    }
 }
 window.addEventListener('load', getLocalStorage)
 
@@ -250,31 +197,18 @@ async function getWeather(newSity) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=${lang}&appid=9cdada7fb28c4dfbf39ead2d36a2b20b&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
-    // console.log(data.weather[0].id, data.weather[0].description, data.main.temp);
 
     weatherIcon.className = 'weather-icon owf';
-
     weatherIcon.classList.add(`owf-${data.weather[0].id}`); //иконка погоды
     temperature.textContent = `${data.main.temp}°C`; //температуру в °C
     weatherDescription.textContent = data.weather[0].description; //описание погоды
-
 
     if (lang === 'ru') {
         wind.textContent = `Скорость ветра: ${data.wind.speed} м/с`; //скорость ветра в м/с
         humidity.textContent = `Относительная влажность воздуха: ${data.main.humidity} %`; // относительную влажность воздуха в %
     }
-
-
-
-    // weatherIcon.className = 'weather-icon owf';
 }
 getWeather();
-
-
-
-// changeQuote.addEventListener('click', function() {
-//     console.log(nameUser.value);
-// });
 
 const max = 100
 const min = 1;
@@ -282,25 +216,18 @@ let count = Math.floor(Math.random() * (max - min)) + min;
 let maxCount = 0;
 
 async function getQuotes(lang) {
-
     const quotes = lang === 'ru' ? '../assets/json/quotes.json' : 'https://type.fit/api/quotes';
     const res = await fetch(quotes);
-    // const res = await fetch(quotes);
-    // const data = await res.json();
     return await res.json();
 }
-
-// getQuotes();
 
 async function goGetQuotes(i,lang){
     const data = await getQuotes(lang);
     maxCount = data.length;
-
     setTimeout(() => {
         quotesQuote.textContent = data[i].text;
         quotesAuthor.textContent = data[i].author;
     }, 500);
-
     maxCount >= count ? count++ : count = 0;
 }
 
@@ -308,36 +235,18 @@ async function goGetQuotes(i,lang){
 
 
 goGetQuotes(count,lang);
-
-
 if (quotesButton) {
-
     quotesButton.addEventListener("click", function (e) {
         goGetQuotes(count,lang);
     });
-
 }
 
 
 
 // Audio
 
-// const audio = document.querySelector('audio');
-// const playBtn = document.querySelector('.play');
-//
-// function playAudio() {
-//     audio.currentTime = 0;
-//     audio.play();
-// }
-// function pauseAudio() {
-//     audio.pause();
-// }
-//
-// playBtn.addEventListener('click', playAudio);
-
 import playList from './playList.js';
 console.log(playList);
-
 
 let isPlay = false;
 let playNum = 0;
@@ -351,12 +260,9 @@ const playNextBtn = document.querySelector('.play-next');
 const playListContainer = document.querySelector('.play-list');
 
 function playAudio() {
-    // playBtn.classList.toggle("pause");
-
     console.log(playNum);
     if(!isPlay){
         isPlay = true;
-        // audio.src = "../assets/sounds/Ennio Morricone.mp3";
         audio.src = playList[playNum].src;
         audio.currentTime = 0;
         audio.play();
@@ -380,9 +286,6 @@ playBtn.addEventListener('click', playAudio);
 function playNext(){
     playNum < playList.length - 1 ? playNum++ : playNum=0;
     isPlay = false;
-
-    // playListContainer[playNum].classList.add('item-active');
-    // item-active
     playAudio();
 }
 
@@ -396,20 +299,26 @@ function playPrev(){
 playPrevBtn.addEventListener('click', playPrev);
 playNextBtn.addEventListener('click', playNext);
 
-
-    // for (let i = 0; i < playList.length; i++){
-    //     const li = document.createElement('li');
-    //     li.classList.add('play-item');
-    //     li.textContent = playList[i].title;
-    //     playListContainer.append(li);
-    // }
-
     playList.forEach(el => {
         const li = document.createElement('li');
         li.classList.add('play-item');
         li.textContent = el.title;
         playListContainer.append(li);
     })
+
+
+/* Translate */
+
+let currentlanguage = document.querySelector('.language');
+currentlanguage.addEventListener('click', (e) => {
+
+    lang = e.target.innerText;
+    currentlanguage.querySelectorAll('li a').forEach(el => {
+        el.classList.toggle('active');
+    });
+    getWeather();
+
+});
 
 
 
